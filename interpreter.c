6d52206da1769562a4aef4da88775a1a0e3ee209
Baxi-codes <46259792+Baxi-codes@users.noreply.github.com>
2022-01-17 12:38:39 +0530

    Forced struct for assignment marks

--- interpreter.c
+++ interpreter.c
@@ -7,14 +7,25 @@
 #define MAX_INSTRUCTIONS 10000
 #define MAX_INSTRUCTION_SIZE 40
 
-char* sad_emoji="     .-\"\"\"\"\"\"-.\n   .\'          \'.\n  /   O      O   \\\n :                :\n |                |\n :    .------.    :\n  \\  \'        \'  /\n   \'.          .\'\n     \'-......-\'";
+char* instruction1="આગળ\n";
+char* instruction2="પાછળ\n";
+char* instruction3="વધારો\n";
+char* instruction4="ઘટાડો\n";
+char* instruction5="તપાસો\n";
+char* instruction6="છાપો\n";
+char* instruction7="સંપૂર્ણ છાપો\n";	
+char* instruction8="ઇનપુટ\n";	
+char* instruction9="ડબલ\n";	
+char* sad_emote="     .-\"\"\"\"\"\"-.\n   .\'          \'.\n  /   O      O   \\\n :                :\n |                |\n :    .------.    :\n  \\  \'        \'  /\n   \'.          .\'\n     \'-......-\'";
 
 struct Cell{
 	int value;
+	int index;
+	int type;
 };
 
 char instructions[MAX_INSTRUCTIONS][MAX_INSTRUCTION_SIZE];
-int cells[TOTAL_CELLS];
+struct Cell cells[TOTAL_CELLS];
 
 
 int interpret(FILE* file){
@@ -26,7 +37,7 @@ int interpret(FILE* file){
 	rewind(file);
 	if (line_count>MAX_INSTRUCTIONS)
 	{
-		fprintf(stderr,"Too long program, cant have more than %d instructions sorry\n\n%s",MAX_INSTRUCTIONS,sad_emoji);
+		fprintf(stderr,"Too long program, cant have more than %d instructions sorry\n\n%s",MAX_INSTRUCTIONS,sad_emote);
 		exit(1);
 	}
 	for (int i = 0; i <= line_count; ++i)
@@ -36,15 +47,6 @@ int interpret(FILE* file){
 	}
 	int current_cell=0;
 	int current_instruction=0;
-	char* instruction1="આગળ\n";
-	char* instruction2="પાછળ\n";
-	char* instruction3="વધારો\n";
-	char* instruction4="ઘટાડો\n";
-	char* instruction5="તપાસો\n";
-	char* instruction6="છાપો\n";
-	char* instruction7="સંપૂર્ણ છાપો\n";	
-	char* instruction8="ઇનપુટ\n";	
-	char* instruction9="ડબલ\n";	
 	while(current_instruction<=line_count){
 		if (strcmp(instructions[current_instruction],instruction1)==0)
 		{
@@ -54,37 +56,37 @@ int interpret(FILE* file){
 			current_cell=(current_cell+TOTAL_CELLS-1)%TOTAL_CELLS;
 		}else if (strcmp(instructions[current_instruction],instruction3)==0)
 		{
-			++cells[current_cell];
+			++cells[current_cell].value;
 		}else if (strcmp(instructions[current_instruction],instruction4)==0)
 		{
-			--cells[current_cell];
+			--cells[current_cell].value;
 		}else if (strcmp(instructions[current_instruction],instruction5)==0)
 		{
-			if (cells[current_cell]==0)
+			if (cells[current_cell].value==0)
 			{
-				current_instruction=cells[0];
+				current_instruction=cells[0].value;
 			}else{
-				current_instruction=cells[1];
+				current_instruction=cells[1].value;
 			}
 			continue;
 		}else if (strcmp(instructions[current_instruction],instruction6)==0)
 		{
-			printf("%c",cells[current_cell]);
+			printf("%c",cells[current_cell].value);
 		}else if (strcmp(instructions[current_instruction],instruction7)==0)
 		{
 			for (int i = 0; i < TOTAL_CELLS; ++i)
 			{
-				if (cells[i]!=0)
+				if (cells[i].value!=0)
 				{
-					printf("%c",cells[i]);
+					printf("%c",cells[i].value);
 				}
 			}
 		}else if (strcmp(instructions[current_instruction],instruction8)==0)
 		{
-			scanf("%d",&cells[current_cell]);
+			scanf("%d",&cells[current_cell].value);
 		}else if (strcmp(instructions[current_instruction],instruction9)==0)
 		{
-			cells[current_cell]*=2;
+			cells[current_cell].value*=2;
 		}
 		++current_instruction;
 	}
@@ -92,18 +94,22 @@ int interpret(FILE* file){
 
 int main(int argc, char** argv){
 	if (argc!=2){
-		fprintf(stderr, "incorrect number of arguments\n\n%s",sad_emoji);
+		fprintf(stderr, "incorrect number of arguments\n\n%s",sad_emote);
 		exit(1);
 	}
 	FILE* source=fopen(argv[1],"r");
 	if(!source){
-		fprintf(stderr,"Could not open source file %s\n\n%s",argv[1],sad_emoji);
+		fprintf(stderr,"Could not open source file %s\n\n%s",argv[1],sad_emote);
 		exit(1);
 	}
 	for (int i = 0; i < TOTAL_CELLS; ++i)
 	{
-		cells[i]=0;
+		cells[i].value=0;
+		cells[i].index=i;
+		cells[i].type=2;
 	}
+	cells[0].type=0;
+	cells[1].type=1;
 	interpret(source);
 	return 0;
 }
